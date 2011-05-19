3958bacd095034689f3f1fba152ba671198aee2d
BigBlackBug <jenkoff@gmail.com>
2011-05-19 21:20:10 +0400

    Апдейт вьюшки. С функциональностью всё ок. Как будто бы с базой. Остаолсь толтько вид допилить

---
+++ StoreMore/src/java/qbix/sm/client/services/DownloadServlet.java
@@ -0,0 +1,66 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package qbix.sm.client.services;
+
+import com.google.gwt.user.client.Window;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author BigBlackBug
+ */
+public class DownloadServlet extends HttpServlet
+{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+
+        doDownload(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        super.doGet(req, resp);
+    }
+
+    private void doDownload(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException
+    {
+
+
+        String fileName = (String) req.getParameter("fileName");
+
+        File f = new File(fileName);
+        int length = 0;
+        ServletOutputStream op = resp.getOutputStream();
+        ServletContext context = getServletConfig().getServletContext();
+        String mimetype = context.getMimeType(fileName);
+
+        resp.setContentType((mimetype != null) ? mimetype : "application/octet-stream");
+        resp.setContentLength((int) f.length());
+        resp.setHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
+
+
+        byte[] buffer = new byte[4096];
+        DataInputStream in = new DataInputStream(new FileInputStream(f));
+
+        while ((in != null) && ((length = in.read(buffer)) != -1))
+            op.write(buffer, 0, length);
+
+        in.close();
+        op.flush();
+        op.close();
+    }
+}
---
+++ StoreMore/src/java/qbix/sm/client/services/FileUploadServlet.java
@@ -0,0 +1,86 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package qbix.sm.client.services;
+import gwtupload.server.UploadAction;
+import gwtupload.server.exceptions.UploadActionException;
+import java.io.File;
+
+import java.util.List;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.fileupload.FileItem;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+import qbix.sm.client.services.FCService;
+
+/**
+ *
+ * @author iliax
+ */
+
+public class FileUploadServlet extends UploadAction {
+
+	private static final long serialVersionUID = 1L;
+
+        FCService fCService=null;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+
+        ApplicationContext context = WebApplicationContextUtils.
+            getRequiredWebApplicationContext(getServletContext());
+        fCService=(FCService) context.getBean("FCService");
+    }
+
+
+    @Override
+	public String executeAction(HttpServletRequest request,
+			List<FileItem> sessionFiles) throws UploadActionException {
+		String response = "Received file:";
+
+                File file=new File(".");
+                
+		for (FileItem item : sessionFiles) {
+			if (!item.isFormField()) {
+				try {
+//                                    file = File.createTempFile("receivedFile", ".tmp",
+//                                        new File(file.getCanonicalPath()));
+//					 item.write(file);
+                                    file=new File(file.getCanonicalPath()+"/for_saved_files/"+item.getName());
+                                  //  File tempFile=File.createTempFile("receivedFile_", "", file);
+
+                                    item.write(file);
+					 response += " " + file.getPath();
+
+					response += " " + item.getName() + ", \nsize=  "
+					+ item.getSize() + ", \nContentType= "
+					+ item.getContentType();
+
+				} catch (Exception e) {
+					throw new UploadActionException(e.getMessage());
+				}
+			}
+                        getSessionFileItems(request);
+		}
+
+		try {
+			// Remove files from session
+			removeSessionFileItems(request);
+		} catch (Exception e) {
+			throw new UploadActionException(e.getMessage());
+		}
+
+		// Send information of the received files to the client.
+		return response;
+	}
+
+}
+
